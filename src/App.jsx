e8ff8308c6577ae844033953407fe220d78a3584
Krupali <krupali.lalakiya236@gmail.com>
2025-01-20 12:01:27 +0530

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,22 @@ import Resume from "./pages/Resume";
 import Contact from "./pages/Contact";
 import Navbar from "./navbar/navbar";
 
-
 export default function App() {
   const route = createBrowserRouter([
     {
       path: "/",
       element: (
         <>
-          {" "}
-          <Navbar /> <Home />
+          <div className="container">
+            <div className="grid grid-cols-12 h-screen">
+              <div className="col-span-2">
+                <Navbar />
+              </div>
+              <div className="col-span-10">
+                <Home />
+              </div>
+            </div>
+          </div>
         </>
       ),
     },
@@ -24,8 +31,16 @@ export default function App() {
       path: "/contact",
       element: (
         <>
-          {" "}
-          <Navbar /> <Contact />
+          <div className="container">
+            <div className="grid grid-cols-12 h-screen">
+              <div className="col-span-2">
+                <Navbar />
+              </div>
+              <div className="col-span-10">
+                <Contact />
+              </div>
+            </div>
+          </div>
         </>
       ),
     },
@@ -33,23 +48,53 @@ export default function App() {
       path: "/resume",
       element: (
         <>
-          <Navbar /> <Resume />
+         <div className="container">
+            <div className="grid grid-cols-12 h-screen">
+              <div className="col-span-2">
+                <Navbar />
+              </div>
+              <div className="col-span-10">
+                <Resume />
+              </div>
+            </div>
+          </div>
+          {/* <Navbar /> <Resume /> */}
         </>
       ),
     },
-    {
-      path: "/skill",
-      element: (
-        <>
-          <Navbar /> <Skill />
-        </>
-      ),
-    },
+    // {
+    //   path: "/skill",
+    //   element: (
+    //     <>
+    //        <div className="container">
+    //         <div className="grid grid-cols-12 h-screen">
+    //           <div className="col-span-2">
+    //             <Navbar />
+    //           </div>
+    //           <div className="col-span-10">
+    //             <Skill />
+    //           </div>
+    //         </div>
+    //       </div>
+    //       {/* <Navbar /> <Skill /> */}
+    //     </>
+    //   ),
+    // },
     {
       path: "/portfolio",
       element: (
         <>
-          <Portfolio />
+           <div className="container">
+            <div className="grid grid-cols-12 h-screen">
+              <div className="col-span-2">
+                <Navbar />
+              </div>
+              <div className="col-span-10">
+                <Portfolio />
+              </div>
+            </div>
+          </div>
+          {/* <Portfolio /> */}
         </>
       ),
     },
@@ -57,7 +102,17 @@ export default function App() {
       path: "/about",
       element: (
         <>
-          <About />
+           <div className="container">
+            <div className="grid grid-cols-12 h-screen">
+              <div className="col-span-2">
+                <Navbar />
+              </div>
+              <div className="col-span-10">
+                <About />
+              </div>
+            </div>
+          </div>
+          {/* <About /> */}
         </>
       ),
     },
--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -1,96 +1,102 @@
-// import React from "react";
+import React from "react";
 import home from "./../assets/home.svg";
 import user from "./../assets/user.svg";
 import document from "./../assets/document.svg";
 import portfolio from "./../assets/portfolio.svg";
 import contact from "./../assets/contact.svg";
-
-import { useNavigate } from "react-router-dom";
-// import Navbar from "../navbar/navbar";
+import { NavLink, useNavigate } from "react-router-dom";
 const Navbar = () => {
   const navigate = useNavigate();
   return (
-    <>     
-      <div className="h-full absolute flex justify-center items-center ms-6">
-        <div className=" flex flex-col">
-          <div
-            className="bg-[#0563bb] rounded-full my-3 h-16 w-16 flex items-center justify-start transition-all duration-700 ease-in-out delay-150 
-                                     cursor-pointer   hover:w-40 hover:delay-150 overflow-hidden"
-          >
-            <img
-              src={home}
-              alt="home"
-              height="30px"
-              width="30px"
-              className="m-4 transition-all duration-700 ease-in-out cursor-pointer"
-            />
-            <span className="text-white opacity-100 ml-3 transition-opacity duration-700 ease-in-out font-medium text-xl">
-              Home
-            </span>
-          </div>
-
-          <div
-            className="bg-[#0563bb] rounded-full my-3 h-16 w-16 flex items-center justify-start transition-all duration-700 ease-in-out delay-150 
+    <>
+      <div className="navigation flex items-center">
+        <ul className="flex flex-col ms-6">
+          <NavLink to="/">
+            <li
+              className="bg-[#0563bb] rounded-full my-3 h-16 w-16 flex items-center justify-start transition-all duration-700 ease-in-out delay-150
+                                       hover:w-40 hover:delay-150 overflow-hidden cursor-pointer"
+            >
+              <img
+                src={home}
+                alt="home"
+                height="30px"
+                width="30px"
+                className="m-4 transition-all duration-700 ease-in-out"
+              />
+              <span className="text-white opacity-100 ml-3 transition-opacity duration-700 ease-in-out font-medium text-xl">
+                Home
+              </span>
+            </li>
+          </NavLink>
+          <NavLink to="/about">
+            <li
+              className="bg-[#0563bb] rounded-full my-3 h-16 w-16 flex items-center justify-start transition-all duration-700 ease-in-out delay-150 
+                                        hover:w-40 hover:delay-150 overflow-hidden cursor-pointer"
+            >
+              <img
+                src={user}
+                alt="about"
+                height="30px"
+                width="30px"
+                className="m-4 transition-all duration-700 ease-in-out"
+              />
+              <span className="text-white opacity-100 ml-3 transition-opacity duration-700 ease-in-out font-medium text-xl">
+                About
+              </span>
+            </li>
+          </NavLink>
+          <NavLink to="/resume">
+            <li
+              className="bg-[#0563bb] rounded-full my-3 h-16 w-16 flex items-center justify-start transition-all duration-700 ease-in-out delay-150 
                                         hover:w-40 hover:delay-150 overflow-hidden"
-            //   onClick={navigate("/about")}
-          >
-            <img
-              src={user}
-              alt="about"
-              height="30px"
-              width="30px"
-              className="m-4 transition-all duration-700 ease-in-out"
-            />
-            <span className="text-white opacity-100 ml-3 transition-opacity duration-700 ease-in-out font-medium text-xl">
-              About
-            </span>
-          </div>
-          <div
-            className="bg-[#0563bb] rounded-full my-3 h-16 w-16 flex items-center justify-start transition-all duration-700 ease-in-out delay-150 
+            >
+              <img
+                src={document}
+                alt="resume"
+                height="30px"
+                width="30px"
+                className="m-4 transition-all duration-700 ease-in-out"
+              />
+              <span className="text-white opacity-100 ml-3 transition-opacity duration-700 ease-in-out font-medium text-xl">
+                Resume
+              </span>
+            </li>
+          </NavLink>
+          <NavLink to="/portfolio">
+            <li
+              className="bg-[#0563bb] rounded-full my-3 h-16 w-16 flex items-center justify-start transition-all duration-700 ease-in-out delay-150 
                                         hover:w-40 hover:delay-150 overflow-hidden"
-          >
-            <img
-              src={document}
-              alt="resume"
-              height="30px"
-              width="30px"
-              className="m-4 transition-all duration-700 ease-in-out"
-            />
-            <span className="text-white opacity-100 ml-3 transition-opacity duration-700 ease-in-out font-medium text-xl">
-              Resume
-            </span>
-          </div>
-          <div
-            className="bg-[#0563bb] rounded-full my-3 h-16 w-16 flex items-center justify-start transition-all duration-700 ease-in-out delay-150 
+            >
+              <img
+                src={portfolio}
+                alt="portfolio"
+                height="30px"
+                width="30px"
+                className="m-4 transition-all duration-700 ease-in-out"
+              />
+              <span className="text-white opacity-100 ml-3 transition-opacity duration-700 ease-in-out font-medium text-xl">
+                Portfolio
+              </span>
+            </li>
+          </NavLink>
+          <NavLink to="/contact">
+            <li
+              className="bg-[#0563bb] rounded-full my-3 h-16 w-16 flex items-center justify-start transition-all duration-700 ease-in-out delay-150 
                                         hover:w-40 hover:delay-150 overflow-hidden"
-          >
-            <img
-              src={portfolio}
-              alt="portfolio"
-              height="30px"
-              width="30px"
-              className="m-4 transition-all duration-700 ease-in-out"
-            />
-            <span className="text-white opacity-100 ml-3 transition-opacity duration-700 ease-in-out font-medium text-xl">
-              Portfolio
-            </span>
-          </div>
-          <div
-            className="bg-[#0563bb] rounded-full my-3 h-16 w-16 flex items-center justify-start transition-all duration-700 ease-in-out delay-150 
-                                        hover:w-40 hover:delay-150 overflow-hidden"
-          >
-            <img
-              src={contact}
-              alt="contact"
-              height="30px"
-              width="30px"
-              className="m-4 transition-all duration-700 ease-in-out"
-            />
-            <span className="text-white opacity-100 ml-3 transition-opacity duration-700 ease-in-out font-medium text-xl">
-              Contact
-            </span>
-          </div>
-        </div>
+            >
+              <img
+                src={contact}
+                alt="contact"
+                height="30px"
+                width="30px"
+                className="m-4 transition-all duration-700 ease-in-out"
+              />
+              <span className="text-white opacity-100 ml-3 transition-opacity duration-700 ease-in-out font-medium text-xl">
+                Contact
+              </span>
+            </li>
+          </NavLink>
+        </ul>
       </div>
     </>
   );
