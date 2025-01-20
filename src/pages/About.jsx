e4f7cefeeafb2c5a59e01ff83f6ab5fd30cb09ff
Krupali <krupali.lalakiya236@gmail.com>
2025-01-20 12:33:58 +0530

    filed modified

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,141 @@
 import bg from "./../assets/images/bg.jpg";
 import chevronRight from "./../assets/chevronRight.svg";
 import Skill from "./Skills";
-import Navbar from "../navbar/navbar";
 const About = () => {
   return (
     <>
-      <div className="container:full relative">
+      <div className="grid">
+        <h1 className="text-center font-bold text-[40px] py-6">About</h1>
+        <p className="text-center text-xl my-5">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis
+          distinctio officiis asperiores ad minus cumque vero veritatis quaerat
+          illum autem, neque a alias temporibus quisquam! Hic rem repellat fugit
+          maiores.
+        </p>
+      </div>
+      <div className="grid grid-cols-12 gap-7 my-5">
+        <div className="col-span-3">
+          <img src={bg} alt="bg" className="h-full w-full my-5" />
+        </div>
+        <div className="col-span-9">
+          <h3 className="font-bold text-4xl my-3">Frontend Devloper</h3>
+          <p className="text-2xl my-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, id?
+          </p>
+          <div className="grid grid-cols-2">
+            <div className="columns-1 my-3">
+              <div className="flex my-5">
+                <img
+                  src={chevronRight}
+                  alt="chevronRight"
+                  width="20px"
+                  height="20px"
+                />
+                <p className="font-bold text-xl ps-2">
+                  Birthday :
+                  <span className="font-normal ps-4">23 June 2001</span>{" "}
+                </p>
+              </div>
+              <div className="flex my-5">
+                <img
+                  src={chevronRight}
+                  alt="chevronRight"
+                  width="20px"
+                  height="20px"
+                />
+                <p className="font-bold text-xl ps-2">
+                  Website :
+                  <span className="font-normal ps-4">www.example.com</span>{" "}
+                </p>
+              </div>
+              <div className="flex my-5">
+                <img
+                  src={chevronRight}
+                  alt="chevronRight"
+                  width="20px"
+                  height="20px"
+                />
+                <p className="font-bold text-xl ps-2">
+                  Phone :<span className="font-normal ps-4">93277 77951</span>{" "}
+                </p>
+              </div>
+              <div className="flex my-5">
+                <img
+                  src={chevronRight}
+                  alt="chevronRight"
+                  width="20px"
+                  height="20px"
+                />
+                <p className="font-bold text-xl ps-2">
+                  City :<span className="font-normal ps-4"> Rajkot </span>{" "}
+                </p>
+              </div>
+            </div>
+            <div className="columns-1 my-3">
+              <div className="flex my-5">
+                <img
+                  src={chevronRight}
+                  alt="chevronRight"
+                  width="20px"
+                  height="20px"
+                />
+                <p className="font-bold text-xl ps-2">
+                  Age :<span className="font-normal ps-4">23</span>{" "}
+                </p>
+              </div>
+              <div className="flex my-5">
+                <img
+                  src={chevronRight}
+                  alt="chevronRight"
+                  width="20px"
+                  height="20px"
+                />
+                <p className="font-bold text-xl ps-2">
+                  Degree :<span className="font-normal ps-4">MCA</span>{" "}
+                </p>
+              </div>
+              <div className="flex my-5">
+                <img
+                  src={chevronRight}
+                  alt="chevronRight"
+                  width="20px"
+                  height="20px"
+                />
+                <p className="font-bold text-xl ps-2">
+                  Email :
+                  <span className="font-normal ps-4">
+                    krupali.lalakiya236@gmail.com
+                  </span>{" "}
+                </p>
+              </div>
+              <div className="flex my-5">
+                <img
+                  src={chevronRight}
+                  alt="chevronRight"
+                  width="20px"
+                  height="20px"
+                />
+                <p className="font-bold text-xl ps-2">
+                  Freelance :
+                  <span className="font-normal ps-4">Not available</span>{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="grid">
+            <p className="text-xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+              rerum modi officiis tempora, quaerat odit unde voluptatum illum
+              consequuntur laudantium eos ipsa assumenda, tempore perferendis?
+              Accusamus reprehenderit eveniet tempora facere.
+            </p>
+          </div>
+        </div>
+      </div>
+      <Skill />
+      {/* <div className="container:full relative">
         <div className="grid grid-cols-12 h-screen">
-          {/* <Navbar /> */}
+        
           <div className="col-span-10 flex flex-col justify-center align-middle items-start ms-10 px-5">
             <div className="container mx-auto">
               <div className="grid my-5">
@@ -32,7 +160,7 @@ const About = () => {
                     Dolore, id?
                   </p>
                   <div className="grid grid-cols-2">
-                    <div className="columns-1 my-6">
+                    <div className="columns-1 my-3">
                       <div className="flex my-5">
                         <img
                           src={chevronRight}
@@ -84,7 +212,7 @@ const About = () => {
                         </p>
                       </div>
                     </div>
-                    <div className="columns-1 my-6">
+                    <div className="columns-1 my-3">
                       <div className="flex my-5">
                         <img
                           src={chevronRight}
@@ -152,7 +280,7 @@ const About = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,21 +1,68 @@
-import ProgressCard from './processbar'
-
 const Skill = () => {
   return (
     <>
-      {/* <div className="container mx-auto my-16"> */}
-      {/* </div> */}
-      <div className="grid mt-40">
-        <h1 className="text-center font-bold">Skills</h1>
-        <p className="text-xl text-center my-4">
+      <div className="grid">
+        <h1 className="text-center font-bold my-5">Skills</h1>
+        <p className="text-xl text-center my-2">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex at aut
           porro.
         </p>
       </div>
-      <div className="grid grid-cols-2">
-        <div className="columns-1">
-          <ProgressCard />
+      <div className="grid grid-cols-2 mt-3">
+        <div className="columns-1 my-3">
+          <h1 className="font-semibold text-sm">HTML</h1>
+          <div className="flex items-center">
+            <div className="h-2 w-96 bg-slate-400">
+              <div className="h-2 w-96 bg-blue-900"></div>
+            </div>
+            <span className="ms-5 items-center">100% </span>
+          </div>
         </div>
+        <div className="columns-1 my-3">
+          <h1 className="font-semibold text-sm">CSS</h1>
+          <div className="flex items-center">
+            <div className="h-2 w-96 bg-slate-500">
+              <div className="h-2 w-80 bg-blue-900"></div>
+            </div>
+            <span className="ms-5 items-center">95% </span>
+          </div>
+        </div>
+        <div className="columns-1 my-3">
+          <h1 className="font-semibold text-sm">Javascript</h1>
+          <div className="flex items-center">
+            <div className="h-2 w-96 bg-slate-500">
+              <div className="h-2 w-72 bg-blue-900"></div>
+            </div>
+            <span className="ms-5 items-center">75% </span>
+          </div>
+        </div>
+        <div className="columns-1 my-3">
+          <h1 className="font-semibold text-sm">React</h1>
+          <div className="flex items-center">
+            <div className="h-2 w-96 bg-slate-500">
+              <div className="h-2 w-64 bg-blue-900"></div>
+            </div>
+            <span className="ms-5 items-center">70% </span>
+          </div>
+        </div>
+        {/* <div className="columns-1 my-3">
+          <h1 className="font-semibold text-sm">Photoshop</h1>
+          <div className="flex items-center">
+            <div className="h-2 w-96 bg-slate-500">
+              <div className="h-2 w-64 bg-blue-900"></div>
+            </div>
+            <span className="ms-5 items-center">70% </span>
+          </div>
+        </div>
+        <div className="columns-1 my-3">
+          <h1 className="font-semibold text-sm">Coral draw</h1>
+          <div className="flex items-center">
+            <div className="h-2 w-96 bg-slate-500">
+              <div className="h-2 w-72 bg-blue-900"></div>
+            </div>
+            <span className="ms-5 items-center">75% </span>
+          </div>
+        </div> */}
       </div>
     </>
   );
