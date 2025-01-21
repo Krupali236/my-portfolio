69634fbe098af38538e4c32a956907309bea9a9c
Krupali <krupali.lalakiya236@gmail.com>
2025-01-21 13:56:39 +0530

    commit

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,60 +2,60 @@ const Portfolio = () => {
   const portfolioData = [
     {
       id: 1,
-      title: "Template",
-      Category: "HTML",
-      Description: "Using Html elements",
-      image: "",
+      title: "HTML Form",
+      category: "HTML",
+      description: "Using Html elements",
+      image: "src/assets/images/html-form-11.png",
+      link: "https://github.com/Krupali236/html-form1/tree/main",
     },
     {
       id: 2,
       title: "Template",
-      Category: "HTML",
-      Description: "Using Html elements",
+      category: "HTML",
+      description: "Using Html elements",
       image: "",
     },
     {
       id: 3,
       title: "Template",
-      Category: "HTML",
-      Description: "Using Html elements",
+      category: "HTML",
+      description: "Using Html elements",
       image: "",
     },
     {
       id: 4,
       title: "Template",
-      Category: "HTML",
-      Description: "Using Html elements",
+      category: "HTML",
+      description: "Using Html elements",
       image: "",
     },
     {
       id: 5,
       title: "Template",
-      Category: "HTML",
-      Description: "Using Html elements",
+      category: "HTML",
+      description: "Using Html elements",
       image: "",
     },
     {
       id: 6,
       title: "Template",
-      Category: "HTML",
-      Description: "Using Html elements",
+      category: "HTML",
+      description: "Using Html elements",
       image: "",
     },
   ];
   return (
     <>
-      {/* <h1 className="text-center my-3 font-bold  decoration-2 underline underline-offset-8 ">Portfolio</h1> */}
       <h1 className="text-center font-bold text-[40px] py-3  decoration-2 underline underline-offset-8">
         Portfolio
       </h1>
-      <p className="text-center">
+      <p className="text-center my-4">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, illum!
         Mollitia quas incidunt delectus nobis sequi minima quod aspernatur
         perferendis!
       </p>
 
-      <div className="container my-3">
+      <div className="container mt-10 mb-3">
         <div className="flex justify-center my-5">
           <button className="bg-[#0563bb] text-white w-32 rounded-full mx-4 hover:bg-slate-700">
             All
@@ -74,19 +74,26 @@ const Portfolio = () => {
           </button>
         </div>
 
-        <div className="grid grid-cols-4 my-4">
-          <div className="columns-1">
-            <div className="border-2">
-                <h1 className="text-center">Template</h1>
-                <p className="text-center">Desctiption</p>
-                <p className="text-center">Category : HTml</p>
+        <div className="grid grid-cols-4 my-4 gap-4">
+          {portfolioData.map((ele, ind) => (
+            <div className="columns-1" key={ind}>
+              <div className="border-2 rounded-xl h-full">
+                <img
+                  src={ele.image}
+                  alt={ele.title}
+                  className="h-auto w-full rounded-t-xl"
+                />
+                <h1 className="text-center text-lg font-bold">{ele.title}</h1>
+                <p className="text-center">{ele.description}</p>
+                <p className="text-center">{ele.category}</p>
+                <p className="text-center">
+                  <a className="font-normal text-slate-700" href={ele.link}>
+                    Git Link
+                  </a>
+                </p>
+              </div>
             </div>
-            {/* {portfolioData.map((ele) => {
-              <div>{ele.title} </div>;
-              <div>{ele.Category}</div>;
-
-            })} */}
-          </div>
+          ))}
         </div>
       </div>
     </>
