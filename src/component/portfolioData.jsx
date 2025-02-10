01fc2d16fb1bc7a50a888d32b16c6d49e8dd161d
Krupali <krupali.lalakiya236@gmail.com>
2025-02-10 11:13:09 +0530

    portfolio modified - category

---
+++ src/component/portfolioData.jsx
@@ -0,0 +1,173 @@
+export const portfolioData = [
+  {
+    id: 1,
+    title: "Registration Form Template Design",
+    technology: "HTML , CSS",
+    category: "Form",
+    description:
+      "Designed a responsive and user-friendly HTML form with structured input fields, validation, and a modern UI. ",
+    image: "src/assets/images/html-form-1.png",
+    link: "https://github.com/Krupali236/html-form1/tree/main",
+  },
+  {
+    id: 2,
+    title: "Contact Us Form Template Design",
+    technology: "HTML , CSS",
+    category: "Form",
+    description:
+      "Designed a responsive and user-friendly HTML form with structured input fields, validation, and a modern UI using gradient.",
+    image: "src/assets/images/html-form-2.png",
+    link: "https://github.com/Krupali236/html-form2",
+  },
+  {
+    id: 3,
+    title: "Login Form Template Design",
+    technology: "HTML , CSS",
+    category: "Form",
+    description:
+      "Designed a responsive and user-friendly HTML form with structured input fields, validation, and a modern UI. Styled using CSS/Tailwind for better accessibility and enhanced user experience.",
+    image: "src/assets/images/html-form-3.png",
+    link: "https://github.com/Krupali236/html-form3",
+  },
+  {
+    id: 4,
+    title: "Login Template Design",
+    technology: "HTML , CSS",
+    category: "Form",
+    description:
+      "Created visually appealing and responsive CSS form designs and templates with clean layouts, custom styling, and smooth user interactions.",
+    image: "src/assets/images/css-demo-page.png",
+    link: "https://github.com/Krupali236/css-demo-page",
+  },
+  {
+    id: 5,
+    title: "Sign in page Template Design",
+    technology: "HTML , CSS",
+    category: "Form",
+    description:
+      " Built interactive and accessible CSS layouts tailored for various web applications, ensuring pixel-perfect design, smooth transitions, and optimized performance.",
+    image: "src/assets/images/css-signin-page.png",
+    link: "https://github.com/Krupali236/css-SignIn",
+  },
+  {
+    id: 6,
+    title: "Template Design",
+    technology: "HTML , CSS",
+    category: "Form",
+    description:
+      "Styled using CSS for better accessibility and enhanced user experience.",
+    image: "src/assets/images/css-signup-page.png",
+    link: "https://github.com/Krupali236/css-page-template",
+  },
+
+  {
+    id: 7,
+    title: "Wesite Template Design",
+    technology: "HTML , CSS , Bootstrap",
+    category: "Website",
+    description:
+      "Developed dynamic and visually appealing CSS layouts that enhance user experience with intuitive navigation, smooth animations, and optimized performance.",
+    image: "src/assets/images/css-template-design.png",
+    link: "https://github.com/Krupali236/css-website-template",
+  },
+  {
+    id: 8,
+    title: "Modern Chair Layout Template Design",
+    technology: "HTML , CSS, , Bootstrap",
+    category: "Website",
+    description:
+      "Created scalable and reusable CSS layout templates using modern techniques like CSS Grid, Flexbox, and media queries to ensure cross-browser compatibility and responsiveness.",
+    image: "src/assets/images/css-wesite-template.png",
+    link: "https://github.com/Krupali236/css-modern-chair-layout",
+  },
+  {
+    id: 9,
+    title: "Template Design",
+    technology: "HTML , CSS, Bootstrap",
+    category: "Website",
+    description:
+      "Built interactive and accessible CSS layouts tailored for various web applications, ensuring pixel-perfect design, smooth transitions, and optimized performance.",
+    image: "src/assets/images/css-template.png",
+    link: "https://github.com/Krupali236/css-template-eduGrids",
+  },
+  {
+    id: 10,
+    title: "Template Design",
+    technology: "HTML , CSS , Bootstrap",
+    category: "Website",
+    description:
+      "Developed elegant and fully responsive CSS form templates with custom styling, smooth animations, and intuitive layouts.",
+    image: "src/assets/images/css-template-softypinko.png",
+    link: "https://github.com/Krupali236/css-template-shoftypinko",
+  },
+  {
+    id: 11,
+    title: "Template Design",
+    technology: "HTML , CSS , Bootstrap",
+    category: "Website",
+    description:
+      "Built interactive and accessible CSS layouts tailored for various web applications, ensuring pixel-perfect design, smooth transitions, and optimized performance.",
+    image: "src/assets/images/css-template-inance.png",
+    link: "https://github.com/Krupali236/css-template-inance",
+  },
+  {
+    id: 12,
+    title: "Template Design",
+    technology: "HTML , CSS",
+    category: "Website",
+    description:
+      "Created scalable and reusable CSS layout templates using modern techniques like CSS Grid, Flexbox, and media queries to ensure cross-browser compatibility and responsiveness.",
+    image: "src/assets/images/css-template-mdrn.png",
+    link: "https://github.com/Krupali236/css-template-mdrn",
+  },
+  {
+    id: 13,
+    title: "Sign in page Template Design",
+    technology: "HTML , CSS , Bootstrap",
+    category: "Website",
+    description:
+      "Engineered well-structured and responsive web layouts with CSS, focusing on usability, accessibility, and seamless user interaction across all screen sizes.",
+    image: "src/assets/images/css-template-lugx.png",
+    link: "https://github.com/Krupali236/css-template-lugx",
+  },
+  {
+    id: 14,
+    title: "Calculator",
+    technology: "HTML , CSS , Bootstrap",
+    category: "Application",
+    description:
+      "Developed a fully functional JavaScript calculator with a clean UI, real-time calculations, and error handling. Designed for responsiveness and smooth user interaction using HTML, CSS, and vanilla JavaScript.",
+    image: "src/assets/images/js-calculator.png",
+    link: "https://github.com/Krupali236/calculator",
+  },
+  {
+    id: 15,
+    title: "Baby Outlet",
+    technology: "HTML , CSS , Bootstrap",
+    category: "Application",
+    description:
+      "Created a dynamic JavaScript template with a built-in filtering system, allowing users to search and sort data effortlessly. Designed for a smooth, interactive experience with using vanilla JavaScript.",
+    image: "src/assets/images/js-babyoutlet-template.png",
+    link: "https://github.com/Krupali236/js-babyoutlet-template",
+  },
+  {
+    id: 16,
+    title: "Random Quote",
+    technology: "HTML , CSS , Bootstrap",
+    category: "Application",
+    description:
+      "Built a JavaScript-powered random quote generator that displays a new inspirational quote each time a button is clicked. Integrated smooth transitions and an intuitive user interface for an engaging experience.",
+    image: "src/assets/images/js-random-quote.png",
+    link: "https://github.com/Krupali236/js-random-quote",
+  },
+  {
+    id: 17,
+    title: "Todo List",
+    technology: "HTML , CSS , Bootstrap",
+    category: "Application",
+    description:
+      "Developed a simple and interactive JavaScript Todo app that allows users to add, edit, and delete tasks. Features include task status toggling, local storage for persistence, and a clean, user-friendly interface.",
+    image: "src/assets/images/react-todo.png",
+    link: "https://github.com/Krupali236/react-todolist",
+  },
+];
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,148 +1,9 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {portfolioData} from "../component/portfolioData";
 const Portfolio = () => {
   const navigate = useNavigate();
-  const portfolioData = [
-    {
-      id: 1,
-      title: "HTML Registration Form",
-      category: "HTML",
-      description:
-        "Designed a responsive and user-friendly HTML form with structured input fields, validation, and a modern UI. ",
-      image: "src/assets/images/html-form-1.png",
-      link: "https://github.com/Krupali236/html-form1/tree/main",
-    },
-    {
-      id: 2,
-      title: "HTML Contact Us Form",
-      category: "HTML",
-      description:
-        "Designed a responsive and user-friendly HTML form with structured input fields, validation, and a modern UI using gradient.",
-      image: "src/assets/images/html-form-2.png",
-      link: "https://github.com/Krupali236/html-form2",
-    },
-    {
-      id: 3,
-      title: "HTML Login Form",
-      category: "HTML",
-      description:
-        "Designed a responsive and user-friendly HTML form with structured input fields, validation, and a modern UI. Styled using CSS/Tailwind for better accessibility and enhanced user experience.",
-      image: "src/assets/images/html-form-3.png",
-      link: "https://github.com/Krupali236/html-form3",
-    },
-    {
-      id: 4,
-      title: "CSS Login Template",
-      category: "CSS",
-      description: "Created visually appealing and responsive CSS form designs and templates with clean layouts, custom styling, and smooth user interactions. ",
-      image: "src/assets/images/css-demo-page.png",
-      link: "https://github.com/Krupali236/css-demo-page",
-    },
-    {
-      id: 5,
-      title: "CSS Sign in page template",
-      category: "CSS",
-      description: " Built interactive and accessible CSS layouts tailored for various web applications, ensuring pixel-perfect design, smooth transitions, and optimized performance.",
-      image: "src/assets/images/css-signin-page.png",
-      link: "https://github.com/Krupali236/css-SignIn",
-    },
-    {
-      id: 6,
-      title: "CSS Template",
-      category: "CSS",
-      description: "Styled using CSS for better accessibility and enhanced user experience.",
-      image: "src/assets/images/css-signup-page.png",
-      link: "https://github.com/Krupali236/css-page-template",
-    },
-    {
-      id: 7,
-      title: "CSS wesite template",
-      category: "CSS",
-      description: "Developed dynamic and visually appealing CSS layouts that enhance user experience with intuitive navigation, smooth animations, and optimized performance.",
-      image: "src/assets/images/css-template-design.png",
-      link: "https://github.com/Krupali236/css-website-template",
-    },
-    {
-      id: 8,
-      title: "CSS Modern Chair Layout Template",
-      category: "CSS",
-      description: "Created scalable and reusable CSS layout templates using modern techniques like CSS Grid, Flexbox, and media queries to ensure cross-browser compatibility and responsiveness.",
-      image: "src/assets/images/css-wesite-template.png",
-      link: "https://github.com/Krupali236/css-modern-chair-layout",
-    },
-    {
-      id: 9,
-      title: "CSS Template",
-      category: "CSS",
-      description: "Built interactive and accessible CSS layouts tailored for various web applications, ensuring pixel-perfect design, smooth transitions, and optimized performance.",
-      image: "src/assets/images/css-template.png",
-      link: "https://github.com/Krupali236/css-template-eduGrids",
-    },
-    {
-      id: 10,
-      title: "CSS Template",
-      category: "CSS",
-      description: "Developed elegant and fully responsive CSS form templates with custom styling, smooth animations, and intuitive layouts.",
-      image: "src/assets/images/css-template-softypinko.png",
-      link: "https://github.com/Krupali236/css-template-shoftypinko",
-    },
-    {
-      id: 11,
-      title: "CSS Template",
-      category: "CSS",
-      description: "Built interactive and accessible CSS layouts tailored for various web applications, ensuring pixel-perfect design, smooth transitions, and optimized performance.",
-      image: "src/assets/images/css-template-inance.png",
-      link: "https://github.com/Krupali236/css-template-inance",
-    },
-    {
-      id: 12,
-      title: "CSS Template",
-      category: "CSS",
-      description: "Created scalable and reusable CSS layout templates using modern techniques like CSS Grid, Flexbox, and media queries to ensure cross-browser compatibility and responsiveness.",
-      image: "src/assets/images/css-template-mdrn.png",
-      link: "https://github.com/Krupali236/css-template-mdrn",
-    },
-    {
-      id: 13,
-      title: "CSS Sign in page template",
-      category: "CSS",
-      description: "Engineered well-structured and responsive web layouts with CSS, focusing on usability, accessibility, and seamless user interaction across all screen sizes.",
-      image: "src/assets/images/css-template-lugx.png",
-      link: "https://github.com/Krupali236/css-template-lugx",
-    },
-    {
-      id: 14,
-      title: "Calculator",
-      category: "Javascript",
-      description: "Developed a fully functional JavaScript calculator with a clean UI, real-time calculations, and error handling. Designed for responsiveness and smooth user interaction using HTML, CSS, and vanilla JavaScript.",
-      image: "src/assets/images/js-calculator.png",
-      link: "https://github.com/Krupali236/calculator",
-    },
-    {
-      id: 15,
-      title: "Javascript Template",
-      category: "Javascript",
-      description: "Created a dynamic JavaScript template with a built-in filtering system, allowing users to search and sort data effortlessly. Designed for a smooth, interactive experience with using vanilla JavaScript.",
-      image: "src/assets/images/js-babyoutlet-template.png",
-      link: "https://github.com/Krupali236/js-babyoutlet-template",
-    },
-    {
-      id: 16,
-      title: "Random Quote",
-      category: "Javascript",
-      description: "Built a JavaScript-powered random quote generator that displays a new inspirational quote each time a button is clicked. Integrated smooth transitions and an intuitive user interface for an engaging experience.",
-      image: "src/assets/images/js-random-quote.png",
-      link: "https://github.com/Krupali236/js-random-quote",
-    },
-    {
-      id: 17,
-      title: "Todo List",
-      category: "React",
-      description: "Developed a simple and interactive JavaScript Todo app that allows users to add, edit, and delete tasks. Features include task status toggling, local storage for persistence, and a clean, user-friendly interface.",
-      image: "src/assets/images/react-todo.png",
-      link: "https://github.com/Krupali236/react-todolist",
-    },
-  ];
+ 
   const [filteredData, setFilteredData] = useState(portfolioData); // Use filteredData to store the filtered results
   const [activeCategory, setActiveCategory] = useState("all"); // Track the active category
   const handleClick = (category) => {
@@ -183,36 +44,36 @@ const Portfolio = () => {
           </button>
           <button
             className={`bg-[#0563bb] text-white w-32 rounded-full mx-4 hover:bg-slate-700 ${
-              activeCategory === "HTML" ? "bg-slate-700" : ""
+              activeCategory === "Form" ? "bg-slate-700" : ""
             }`}
-            onClick={() => handleClick("HTML")}
+            onClick={() => handleClick("Form")}
           >
-            HTML
+            Form
           </button>
           <button
             className={`bg-[#0563bb] text-white w-32 rounded-full mx-4 hover:bg-slate-700 ${
-              activeCategory === "CSS" ? "bg-slate-700" : ""
+              activeCategory === "Website" ? "bg-slate-700" : ""
             }`}
-            onClick={() => handleClick("CSS")}
+            onClick={() => handleClick("Website")}
           >
-            CSS
+            Website
           </button>
           <button
             className={`bg-[#0563bb] text-white w-32 rounded-full mx-4 hover:bg-slate-700 ${
-              activeCategory === "Javascript" ? "bg-slate-700" : ""
+              activeCategory === "Application" ? "bg-slate-700" : ""
             }`}
-            onClick={() => handleClick("Javascript")}
+            onClick={() => handleClick("Application")}
           >
-            Javascript
+            Application
           </button>
-          <button
+          {/* <button
             className={`bg-[#0563bb] text-white w-32 rounded-full mx-4 hover:bg-slate-700 ${
               activeCategory === "React" ? "bg-slate-700" : ""
             }`}
             onClick={() => handleClick("React")}
           >
             React
-          </button>
+          </button> */}
         </div>
 
         <div className="grid grid-cols-3 my-4 gap-10">
@@ -231,7 +92,7 @@ const Portfolio = () => {
                 <p className=" ps-3 my-3">
                   {" "}
                   <span className="font-semibold">Technologies : </span>{" "}
-                  {ele.category}
+                  {ele.technology}
                 </p>
                 <p className=" ps-3 my-3">
                   <a className="font-semibold text-slate-700" href={ele.link}>
